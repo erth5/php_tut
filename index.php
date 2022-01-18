e33526c78547c3b6e2eb7710f9ea86cca348758a
Eggo5 <Rthem@gmx.de>
2022-01-18 18:21:00 +0100

    arrays_1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $greeting = 'Hello World';
-
+$own = htmlspecialchars($_GET['my']);
 
 $names = [
         'jefrey', 'John', 'Mary'
--- index.view.php
+++ index.view.php
@@ -22,14 +22,41 @@
     </h1>
 </header>
 
+    <!-- Variant 0 -->
 <?php
     # imported from index.php, no error
     foreach ($names as $name){
         # . means +
         echo $name . ', ';
 }
-
 ?>
+
+    <!-- Variant 1 -->
+<ul>
+    <?php
+    foreach ($names as $name) {
+        # " required instead of '
+        echo "<li>$name</li>";
+    }
+    ?>
+</ul>
+
+    <!-- Variant 2, more option to style -->
+<ul>
+    <?php foreach ($names as $name) : ?>
+    <li><?= $name; ?></li>
+    <?php endforeach; ?>
+</ul>
+
+
+<!-- own tag -->
+<?php
+if ($own = '?')
+    echo TRUE;
+?>
+
+
+
 
 </body>
 </html>

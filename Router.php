e76242b485ecd094c2ca352ce84e1d588299f5f2
Eggo5 <Rthem@gmx.de>
2022-03-08 01:17:44 +0100

    static example

--- Router.php
+++ Router.php
@@ -7,6 +7,22 @@ class Router{
         $this->routes = $routes;
     }
 
+    // this is no instance method, it is a static method
+    // here
+    public static function load($file){
+
+        // create instance of router
+        $router = new static;
+
+        require $file;
+
+        // because is static, it cannot access to object "this"
+        //return $this;
+
+        // have to use previous created instance
+        return $router;
+    }
+
     public function direct($uri){
         // redirecting
         if (array_key_exists($uri, $this->routes)){
